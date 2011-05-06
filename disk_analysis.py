c4c3847c23b664159e9469fddf747d9cc21832a0
Bhargav <vaidya@aida114>
2011-05-06 07:26:54 +0200

    modified disk_analysys

--- disk_analysis.py
+++ disk_analysis.py
@@ -14,6 +14,27 @@ from scipy import integrate
 #
 ################################################################
 
+
+def disk_reso(Data,ul=None):
+    print " This pro requires to have unit length of simumation."
+    print "By Default its taken to be 1 AU"
+    if ul==None : ul=1.0
+    
+    Delr = Data.dx1.max()*ul
+    Deltheta = Data.x1[Data.n1-1]*Data.dx2.min()*ul
+    DelPhi = Data.x1[Data.n1-1]*np.sin(Data.x2[0.5*Data.n2])*Data.dx3.min()*ul
+    Rat1 = Delr/Delr
+    Rat2 = Deltheta/Delr
+    Rat3 = DelPhi/Delr
+    print '-----------ANALYSIS RESULTS----------------'
+    print 'Delta R = %f in the units of scale length ul'%(Delr)
+    print 'Delta theta = %f in the units of scale length ul'%(Deltheta)
+    print 'Delta phi = %f in the units of scale length ul'%(DelPhi)
+    print 'THUS the ratio is :%f:%f:%f'%(Rat1,Rat2,Rat3)
+    print '--------------------------------------------'
+
+
+
 def Analysis(filepath=None,info=None):
     Values = np.asarray(asc.open(filepath+'analysis.out'))
     [a,b]=Values.shape
