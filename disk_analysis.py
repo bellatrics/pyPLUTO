56b3a2babfe6287f91895969920e4798405f1cfe
Bhargav Vaidya <vaidya@mpia.de>
2011-06-21 12:07:36 +0200

    Modified Disk_analysis IV

--- disk_analysis.py
+++ disk_analysis.py
@@ -112,7 +112,7 @@ class Vol_Average(object):
             D = pp.pload(ns,w_dir=wdir)
             dV = np.zeros(D.rho.shape)
             Sigma = np.zeros(D.rho.shape)
-            dV = D.x1[:,np.newaxis,np.newaxis]*D.x1[:,np.newaxis,np.newaxis]*np.sin(D.x2[np.newaxis,:,np.newaxis])*D.dx1[:,np.newaxis,np.newaxis]*D.dx2[np.newaxis,:,np.newaxis]*D.dx3[np.newaxis,:,np.newaxis]
+            dV = D.x1[:,np.newaxis,np.newaxis]*D.x1[:,np.newaxis,np.newaxis]*np.sin(D.x2[np.newaxis,:,np.newaxis])*D.dx1[:,np.newaxis,np.newaxis]*D.dx2[np.newaxis,:,np.newaxis]*D.dx3[np.newaxis,np.newaxis,:]
             Sigma = (D.rho*dV)*(D.rho*D.x1[:,np.newaxis,np.newaxis]*np.sin(D.x2[np.newaxis,:,np.newaxis])*D.dx2[np.newaxis,:,np.newaxis])
             Csound = (D.rho*dV)*(np.sqrt(kwargs.get('Gammae',1.0001)*(D.pr/D.rho)))
             Omega = (D.rho*dV)*(D.v3/D.x1[:,np.newaxis,np.newaxis])
